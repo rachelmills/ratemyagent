08b2a092c20ee458217df6e1646a03b64a05e855
Rachel Mills <rachel.h.mills@gmail.com>
2017-01-05 21:46:27 +0100

    Add log in user to create_agents_test and fix deprecated methods.

--- test/integration/create_agents_test.rb
+++ test/integration/create_agents_test.rb
@@ -3,20 +3,19 @@ require 'test_helper'
 class CreateAgentsTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = User.create(username: "Test User", email: "email@email.com", password: "password")
-    @agent = Agent.create(name: 'Test agent', suburb: 'Nundah', user_id: @user.id)
+    @user = User.create(username: 'Test User', email: 'email@email.com', password: 'password')
   end
 
   test 'get new agent form and create agent' do
+    sign_in_as(@user, 'password')
     get new_agent_path
     assert_template 'agents/new'
-    assert_difference 'Agents.count', 1 do
-      post_via_redirect agents_path, agent: {name: 'Test agent', suburb: 'Nundah'}
+    assert_difference 'Agent.count', 1 do
+      post agents_path, params: {agent: {name: 'Test agent', suburb: 'Nundah'}}
+      follow_redirect!
     end
     assert_template 'agents/show'
-    puts 'where am i'
-    puts response.body
-    assert_match "abcde", response.body
-    assert_match "Nundah", response.body
+    assert_match 'Test agent', response.body
+    assert_match 'Nundah', response.body
   end
 end
