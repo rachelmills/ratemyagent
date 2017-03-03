692ce485c58b7d5b31dcd39ca0ff9c659d46e941
Rachel Mills <rachel.h.mills@gmail.com>
2017-03-03 09:19:38 +0100

    Added up and down arrows for sorted column headers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,6 +27,7 @@ module ApplicationHelper
     else
       direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
     end
-    link_to title, :sort => column, :direction => direction
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
   end
 end
